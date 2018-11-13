113d1504596beba5f7dd20a486b69a27662ae392
Ryan Stong <descartez@github.com>
2018-11-13 08:12:31 -0700

    adds in random to play random sound file

--- radio.py
+++ radio.py
@@ -1,9 +1,10 @@
 from pygame import mixer
+import random
 
 sound_filepath = "/home/pi/Desktop/escape-radio/sounds/"
-sound_files = []
+sound_files = ["Louis Armstrong 1.mp3","Theater Of Hits Oklahoma.mp3"]
 message_sound = ""
 
 mixer.init()
-mixer.music.load(sound_filepath + "Louis Armstrong 1.mp3")
+mixer.music.load(sound_filepath + sound_files[random.randint(0,(len(sound_files)-1))])
 mixer.music.play()
